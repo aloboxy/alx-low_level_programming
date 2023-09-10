258055604245a0ee3e1ec6e3d515a8f46be8db85
aloboxy <alieumbockarie@gmail.com>
2023-09-10 05:27:26 -0700

    c with printf in betty style

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * the printf function in c with main and return void
+ **/
+
+int main(void)
+
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n")
+
+		return (0);
+}
