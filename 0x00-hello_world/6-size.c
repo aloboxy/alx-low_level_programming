ccb057db6d6a1868bae0a38897e1ff9ef986f6fe
aloboxy <alieumbockarie@gmail.com>
2023-09-11 02:21:05 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,7 @@ int main(void)
 	printf("Size of an init: %d byte(s)\n", sizeof(int));
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	printf("Size of a long long: %d byte(s)\n", sizeof(long long int));
+
 	return (0);
 
 }
