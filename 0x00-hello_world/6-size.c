2384f6bcbf030c87ef8de1cbd4b8ef79247ec4af
aloboxy <alieumbockarie@gmail.com>
2023-09-11 01:39:36 -0700

    C code to print all variable type size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main- this the void
+ * Return- return nothing at the end
+ * */
+
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long init));
+	printf("Size of an init: %d byte(s)\n", sizeof(int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	printf("Size of a long long: %d byte(s)\n", sizeof(long long int));
+	return (0);
+
+}
