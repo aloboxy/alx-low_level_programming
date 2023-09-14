7300fe4847b73c6ba6964b13714a2398bdda91f7
aloboxy <alieumbockarie@gmail.com>
2023-09-13 17:07:23 -0700

    c is different in language

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _islower - check for lower case letter
+ * _islower - check for lower case letter if Yes it returns 1
  * @c : character to check the case
- * Return : 0 or 1
+ * Return : 0  if successful. _islower - Entry point 
  */
 
 int _islower(int c)
