fd5679eac4f2371a35998d295ec1705539b6d267
aloboxy <alieumbockarie@gmail.com>
2023-09-13 18:03:17 -0700

    doing addition in c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Entry point for the addition
+ * @k: first value for adding
+ * @m: second value for adding
+ */
+
+int add(int k, int m)
+
+{
+	return (k + m);
+}
